ce939700dae63be2f29480df663daf334879b93d
DiogoSales07 <dsales289@poli.ufrj.br>
2022-01-26 16:34:59 -0300

    Estudando funçoes em js

---
+++ script.js
@@ -0,0 +1,24 @@
+function mensage(){
+    console.log("Esta função está rodando")
+}
+mensage()
+
+function yourname(yourname){
+    console.log(`${yourname}`)
+}
+yourname("Diogo")
+
+function informations(firstname,yourage,kindmusic){
+    console.log(`seu nome é ${firstname}, sua idade é ${yourage} anos e seu estilo de musica favorito é ${kindmusic}`)
+}
+informations("diogo",20,"rap")
+
+function otherinfos(favomovie,favsong){
+    console.log(`Seu filme favorito favorito é ${favomovie} enquanto sua musica favorita é ${favsong}`)
+}
+otherinfos("V de vingança","junho de 94")
+
+function triple(value){
+    console.log(3*value)
+}
+triple(32)
